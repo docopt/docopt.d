8964bb1b76c7556bcb71b00070eafb9bd89432cf
Hiroki Noda <kubo39@gmail.com>
2016-12-15 16:23:52 +0900

    Remove deprecation messages.

--- source/tokens.d
+++ source/tokens.d
@@ -12,7 +12,7 @@ import std.string;
 import std.regex;
 import std.array;
 
-package class Tokens {
+class Tokens {
     string[] _list;
     bool _isParsingArgv;
     this(string[] source, bool parsingArgv = true) {
